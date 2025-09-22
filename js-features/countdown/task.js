b279eed01b906231f6129747eb4c921a87903850
Ekaterina <sindyaeva2001@gmail.com>
2025-09-22 22:53:47 +0400

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,10 @@
+function countDown() {
+    const timer = document.getElementById('timer');
+    timer.textContent --;
+    if (timer.textContent < 0) {
+        clearInterval(id);
+        alert('Вы победили в конкурсе!');
+    }
+}
+
+const id = setInterval(countDown, 1000);
